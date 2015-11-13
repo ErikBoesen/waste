5cdefecb151ff4f59c51fa94ba19bbd706ee7490
Erik Boesen <erik.boesen@gmail.com>
2015-11-12 21:21:22 -0500

    Streamlining. More content coming tomorrow.

--- scripts.js
+++ scripts.js
@@ -1,36 +1,13 @@
-var menu = function() {
-    var isMenuOpen = false;
-    $('.menu-button').click(function() {
-        $(this).toggleClass('open', 200);
-        if (isMenuOpen === false) {
-            $('nav').stop();
-            $('nav').animate({
-                left: '6vw'
-            }, 200);
-            $('.menu-cover').animate({
-                bottom: '0'
-            }, 150);
-
-            isMenuOpen = true;
-        }
-        else {
-            $('nav').stop();
-            $('nav').animate({
-                left: '100vw'
-            }, 200);
-            $('.menu-cover').animate({
-                bottom: '100vh'
-            }, 150);
-
-            isMenuOpen = false;
-        }
-    });
-};
-
-addEventListener('load', menu);
-
 $(document).ready(function() {
-    $('h2:last-child').click(function() {
+    $('button').click(function() {
+        console.log('tests');
         $(window).scrollTop();
+    });
+});
+
+$(window).scroll(function() {
+    var wScroll = $(window).scrollTop();
+    $('.bg').css({
+        'transform': 'translate(0,-' + wScroll / 2 + 'px)'
     });
 });
