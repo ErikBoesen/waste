8424d1389a20b177f7fa5a4ea67bff7317be6682
Erik Boesen <erik.boesen@gmail.com>
2015-11-13 20:12:36 -0500

    More content, more things.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,6 @@
-$(document).ready(function() {
-    $('button').click(function() {
-        console.log('tests');
-        $(window).scrollTop();
-    });
-});
-
-$(window).scroll(function() {
-    var wScroll = $(window).scrollTop();
-    $('.bg').css({
-        'transform': 'translate(0,-' + wScroll / 2 + 'px)'
-    });
+$(window).scroll(function () {
+	var wScroll = $(window).scrollTop();
+	$('.bg').css({
+		'transform': 'translate(0,-' + wScroll * 0.8 + 'px)'
+	});
 });
